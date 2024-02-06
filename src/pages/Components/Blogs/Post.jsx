f60412cc1aa5a6250dfa5215f2441849172672e0
SMThapa <siddharthmthapa@gmail.com>
2024-02-06 23:44:13 +0530

    updates

--- src/pages/Components/Blogs/Post.jsx
+++ src/pages/Components/Blogs/Post.jsx
@@ -1,45 +1,40 @@
+import { Link } from "react-router-dom"
 export const Post = () => {
   return (
     <div className="blog-post">
-
       <main1>
-
-        <a className="post-thumbnail" href="https://wpbingosite.com/wordpress/furnive/2022/05/30/easy-fixes-for-home-decor/" title="Easy Fixes For Home Decor">
+        <Link to={'/blogs/single-blog/69'} className="post-thumbnail" title="Easy Fixes For Home Decor">
           <img className="pic" loading="lazy"  src="https://wpbingosite.com/wordpress/furnive/wp-content/uploads/2018/05/1.jpg" alt="" decoding="async" />
           <button className="button">
-            <a href="https://wpbingosite.com/wordpress/furnive/category/dining-kitchen/"></a>
+            <a ></a>
             Dining &amp; Kitchen
           </button>
-        </a>
+        </Link>
 
       </main1>
       <aside1>
 
         <div className="text">
 
-          <a className="text_input" href="https://wpbingosite.com/wordpress/furnive/2022/05/30/easy-fixes-for-home-decor/">
+          <a className="text_input">
             <time className="date" >September 30, 2022</time>
           </a>
 
-          <a className="text_input" href="https://wpbingosite.com/wordpress/furnive/2022/05/30/easy-fixes-for-home-decor/">
+          <a className="text_input">
             <h3 className="head1">Easy Fixes For Home Decor</h3>
           </a>
 
-          <a className="desc" href="https://wpbingosite.com/wordpress/furnive/2022/05/30/easy-fixes-for-home-decor/">
+          <a className="desc">
             <p className="desc">Our goal has always been to motivate, encourage and
               release our fellow creatives to do their thing  Sed a...</p>
           </a>
 
-          <a className="read_more" href="https://wpbingosite.com/wordpress/furnive/2022/05/30/easy-fixes-for-home-decor/">
+          <Link to={'/blogs/single-blog/96'} className="read_more">
             Read More
-          </a>
+          </Link>
 
         </div>
-
       </aside1>
-
-
-
     </div>
   )
 }
